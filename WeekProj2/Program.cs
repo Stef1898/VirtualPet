e7f8445b82afd4d67b715c3dcc4eb2666e3bd86f
Stef1898 <Stef183@yahoo.com>
2017-04-08 23:40:16 -0400

    initial commit

---
+++ WeekProj2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekProj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a new instance of pet
+            VirtualPet myPet = new VirtualPet();
+
+            //Greeting!
+            Console.WriteLine("Hello and congratulations on your newest pet!  First things first, please give \nyour pet a name!");
+            myPet.name = Console.ReadLine();
+
+            Console.WriteLine("\nGreat!  I'm sure you and " + myPet.name + " will get along great!  Select any number 1-10 to \nbegin!");
+
+            int selectedOption;
+
+            do
+            {
+
+                //TODO Add more options to interact with the pet
+                Console.WriteLine();
+                Console.WriteLine("\nPlease select an option: ");
+                Console.WriteLine("1. Feed the pet");
+                Console.WriteLine("2. Give the pet water");
+                Console.WriteLine("3. Take pet to doctor when health drops below 50!");
+                Console.WriteLine("4. Play with the pet");
+                Console.WriteLine("10. Quit");
+
+                bool trueF;
+                trueF = int.TryParse(Console.ReadLine(), out selectedOption);
+
+                myPet.MyPetStatus();
+
+
+                while (trueF == false)
+                {
+                    Console.Write("Invalid option selected, try again!  Please select an option: \n");
+                    trueF = int.TryParse(Console.ReadLine(), out selectedOption);
+                }
+                if (trueF == true)
+                {
+                    switch (selectedOption)
+                    {
+                        //when the user selects option one we feed the pet
+                        case 1:
+
+                            myPet.HungerDecrease();
+                            Console.WriteLine("\nThank you for feeding " + myPet.name);
+                            break;
+
+                        case 2:
+
+                            myPet.ThirstDecrease();
+                            Console.WriteLine("\n" + myPet.name + " really seemed to appreciate the water!");
+                            break;
+                        case 3:
+                            if (myPet.health >= 36 && myPet.health <= 50)
+                            {
+                                myPet.HealthIncrease();
+                                Console.WriteLine("\n" + myPet.name + " is feeling much better after that checkup!");
+                                break;
+                            }
+                            else if (myPet.health <= 35)
+                            {
+                                myPet.HealthIncrease();
+                                Console.WriteLine("\nThat was a close call " + myPet.name + " is feeling much better now!");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nThat's not necessary right now " + myPet.name + " is feeling perfectly fine!");
+                                break;
+                            }
+                        case 4:
+
+                            myPet.BoredomDecrease();
+                            Console.WriteLine("\nAw! " + myPet.name + " loves playing with you!");
+                            break;
+
+                        //TODO Add more cases for the other ways to interact with our pet
+
+                        case 10:
+
+                            Console.WriteLine("\nThank you for playing.");
+                            break;
+
+                        default:
+
+                            Console.WriteLine("\nInvalid option selected.");
+                            break;
+                    }
+                }
+
+
+                //Put method calls here so the pet can have values change automatically.
+
+                myPet.HungerIncrease();
+                myPet.ThirstIncrease();
+                myPet.HealthDecrease();
+                myPet.BoredomIncrease();
+
+
+            } while (selectedOption != 10);
+
+       
+
+
+        }
+    }
+}
---
+++ WeekProj2/VirtualPet.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekProj2
+{
+    class VirtualPet
+    {
+        public string name;
+        private int hunger;
+        private int thirst;
+        public int health;
+        private int boredom;
+
+        //Default Constructor
+        public VirtualPet()
+        {
+            this.name = "Bob";
+            this.hunger = 20;
+            this.thirst = 20;
+            this.boredom = 20;
+            this.health = 100;
+            //TODO account for more fields
+        }
+
+        public VirtualPet(string name, int hunger, int thirst, int health, int sick, int boredom)
+        {
+            this.name = name;
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.health = health;
+            this.boredom = boredom;
+        }
+
+        //Increase the hunger
+        public void HungerIncrease()
+        {
+            //Modify the values
+            this.hunger = hunger + 3;
+        }
+
+        //Decrease the hunger
+        public void HungerDecrease()
+        {
+            //Modify the values
+            this.hunger = hunger - 5;
+        }
+
+        //Increase the thirst
+        public void ThirstIncrease()
+        {
+            //Modify the values
+            this.thirst = thirst + 3;
+        }
+
+        //Decrease the thirst
+        public void ThirstDecrease()
+        {
+            //Modify the values
+            this.thirst = thirst - 5;
+        }
+
+        //Increase the health
+        public void HealthIncrease()
+        {
+            //Modify the values
+            this.health = health + 65;
+        }
+
+        //Decrease the health
+        public void HealthDecrease()
+        {
+            //Modify the values
+            this.health = health - 3;
+        }
+
+        //Increase boredom
+        public void BoredomIncrease()
+        {
+            //Modify the values
+            this.boredom = boredom + 3;
+        }
+
+        //Decrease boredom
+        public void BoredomDecrease()
+        {
+            //Modify the values
+            this.boredom = boredom - 5;
+        }
+
+
+       
+
+
+        //Stats of virtual pet
+        public void MyPetStatus()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("Hunger: " + hunger);
+            Console.WriteLine("Thirst: " + thirst);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Boredom: " + boredom);
+
+
+        }
+    }
+}
