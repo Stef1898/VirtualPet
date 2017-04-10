8cb7186251b792ee69bc572f8a6736ebe3f141c5
Stef1898 <Stef183@yahoo.com>
2017-04-09 21:50:47 -0400

    major improvements

--- WeekProj2/Program.cs
+++ WeekProj2/Program.cs
@@ -14,29 +14,37 @@ namespace WeekProj2
             VirtualPet myPet = new VirtualPet();
 
             //Greeting!
-            Console.WriteLine("Hello and congratulations on your newest pet!  First things first, please give \nyour pet a name!");
+            Console.WriteLine("Hello and congratulations on your newest pet! First things first, please give \nyour pet a name!");
             myPet.name = Console.ReadLine();
 
-            Console.WriteLine("\nGreat!  I'm sure you and " + myPet.name + " will get along great!  Select any number 1-10 to \nbegin!");
+            Console.WriteLine("\nSplendid! I'm sure you and " + myPet.name + " will get along great! \nSelect any number 1-10 to begin! \n");
 
             int selectedOption;
 
+            bool input;
+            input = myPet.health <= 9 && myPet.hunger >= 100 && myPet.thirst >= 100;
+
             do
             {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine("Your pet's current status is: ");
+                myPet.MyPetStatus();
+                Console.WriteLine("*********************************************\n");
 
-                //TODO Add more options to interact with the pet
+
                 Console.WriteLine();
                 Console.WriteLine("\nPlease select an option: ");
-                Console.WriteLine("1. Feed the pet");
-                Console.WriteLine("2. Give the pet water");
-                Console.WriteLine("3. Take pet to doctor when health drops below 50!");
-                Console.WriteLine("4. Play with the pet");
-                Console.WriteLine("10. Quit");
+                Console.WriteLine("\n1. Chow time!");
+                Console.WriteLine("\n2. Give the pet water");
+                Console.WriteLine("\n3. Play with the pet");
+                Console.WriteLine("\n4. Take pet on a walk!");
+                Console.WriteLine("\n5. Let pet sleep when tiredness is 35 or more");
+                Console.WriteLine("\n6. Let pet outside when bathroom needs are 40 or more");
+                Console.WriteLine("\n7. Take pet to doctor when health drops below 50!");
+                Console.WriteLine("\n10. Quit");
 
                 bool trueF;
                 trueF = int.TryParse(Console.ReadLine(), out selectedOption);
-
-                myPet.MyPetStatus();
 
 
                 while (trueF == false)
@@ -48,48 +56,197 @@ namespace WeekProj2
                 {
                     switch (selectedOption)
                     {
-                        //when the user selects option one we feed the pet
+                        //when the user selects option 1 we feed the pet
                         case 1:
 
-                            myPet.HungerDecrease();
-                            Console.WriteLine("\nThank you for feeding " + myPet.name);
-                            break;
+                            /*Hunger:
+                             -Feed Pet! */
+
+                            if (myPet.hunger <= 0)
+                            {
+                                Console.WriteLine("\nIt looks like " + myPet.name + "'s full for the moment...");
+                                break;
+                            }
+
+                            else if (myPet.hunger >= 75 && myPet.hunger <= 90)
+                            {
+                                Console.WriteLine("\nPoor " + myPet.name + " was so hungry! You should remember to feed " + myPet.name + " more often!");
+                                break;
+                            }
+
+                            else
+                            {
+                                myPet.Meal();
+                                Console.WriteLine("\nThank you for feeding " + myPet.name);
+                                break;
+                            }
+
+
+                        /*Thirst:
+                         -Give pet water! */
 
                         case 2:
+                            if (myPet.thirst <= 0)
+                            {
+                                Console.WriteLine("\nIt looks like " + myPet.name + " doesn't want any more water right now...");
+                                break;
+                            }
 
-                            myPet.ThirstDecrease();
-                            Console.WriteLine("\n" + myPet.name + " really seemed to appreciate the water!");
-                            break;
+                            else if (myPet.thirst >= 75 && myPet.thirst <= 90)
+                            {
+                                Console.WriteLine("\nPoor " + myPet.name + " was so thirsty! You should try to keep " + myPet.name + " well hydrated!");
+                                break;
+                            }
+
+                            else
+                            {
+                                myPet.ThirstDecrease();
+                                Console.WriteLine("\n" + myPet.name + " really seemed to appreciate the water!");
+                                break;
+                            }
+
+
+                        /*Boredom:
+                         * -Play with your pet! Decreases Boredom and Restlessness, but increases thirst and tiredness. */
+
                         case 3:
+
+                            if (myPet.boredom >= 75 && myPet.boredom <= 90)
+                            {
+                                myPet.OverlyBored();
+                                Console.WriteLine("\nNothing like a nice long play session! " + myPet.name + "really wanted to play with you!");
+                                break;
+                            }
+
+                            else if (myPet.restless >= 4 && myPet.restless <= 74)
+                            {
+                                myPet.BoredomDecrease();
+                                Console.WriteLine("\nAw! " + myPet.name + " loves playing with you!");
+                                break;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("\nLooks like " + myPet.name + " isn't interested in playing at the moment");
+                                break;
+                            }
+                            
+                        /*Restlessness:
+                        -Take your pet on a walk! Decreases restlessness and boredom of pet, but increases thirst and tiredness! */
+
+                        case 4:
+
+                            if (myPet.restless >= 75 && myPet.restless <= 90)
+                            {
+                                myPet.OverlyRestless();
+                                Console.WriteLine("\nWoah...it looks like " + myPet.name + "really wanted a walk");
+                                break;
+                            }
+
+                            else if (myPet.restless >= 4 && myPet.restless <= 74)
+                            {
+                                myPet.RestlessDecrease();
+                                Console.WriteLine("\nIt looks like " + myPet.name + " had fun on your walk!");
+                                break;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("\nHm, it looks like " + myPet.name + " is content and doesn't plan on moving right now");
+                                break;
+                            }
+
+
+                        /*Tiredness:
+                        -Let your pet sleep (35 and above)! Decreases tiredness, restlessness and boredom of pet, but increases hunger and thirst!  */
+
+                        case 5:
+                            if (myPet.tired >= 50 && myPet.tired <= 90)
+                            {
+                                myPet.TiredDecrease();
+                                Console.WriteLine("\n" + myPet.name + " is sleeping peacefully!");
+                                break;
+                            }
+
+                            else if (myPet.tired >= 35 && myPet.tired <= 49)
+                            {
+                                myPet.QuickNap();
+                                Console.WriteLine("\n" + myPet.name + " looks refreshed after that nap!");
+                                break;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("\nI looks like " + myPet.name + " just isn't tired right now!");
+                                break;
+                            }
+
+
+                        /*Bathroom Needs:
+                        -Let your pet outside (40 and above)! Decreases pet's bathroom needs!: */
+
+                        case 6:
+                            if (myPet.potty >= 40 && myPet.potty <= 74)
+                            {
+                                myPet.InAndOut();
+                                Console.WriteLine("\nThank you for remembering to let " + myPet.name + " out!");
+                                break;
+                            }
+
+                            else if (myPet.potty >= 75 && myPet.potty <= 90)
+                            {
+                                myPet.PottyDecrease();
+                                Console.WriteLine("\nJust in time!");
+                                break;
+                            }
+
+                            else
+                            {
+                                myPet.ThirstDecrease();
+                                Console.WriteLine("\nLooks like" + myPet.name + " is fine for the moment!");
+                                break;
+                            }
+
+
+                        /*Health: 
+                         -Increase your pet's health by visiting the doctor when health is below 50! 
+                         -Taking care of your pet will also cause slight health increases!*/
+
+                        case 7:
                             if (myPet.health >= 36 && myPet.health <= 50)
                             {
                                 myPet.HealthIncrease();
                                 Console.WriteLine("\n" + myPet.name + " is feeling much better after that checkup!");
                                 break;
                             }
-                            else if (myPet.health <= 35)
+
+                            else if (myPet.health >= 10 && myPet.health <= 35)
                             {
                                 myPet.HealthIncrease();
-                                Console.WriteLine("\nThat was a close call " + myPet.name + " is feeling much better now!");
+                                Console.WriteLine("\nThat was a close call, but " + myPet.name + " will be just fine!");
+                                break;
+                            }
+
+                            else if (myPet.health >= 0 && myPet.health <= 9)
+                            {
+                                Console.WriteLine("\nMy goodness, have your been taking proper care of " + myPet.name + "at all?");
                                 break;
                             }
                             else
                             {
-                                Console.WriteLine("\nThat's not necessary right now " + myPet.name + " is feeling perfectly fine!");
+                                Console.WriteLine("\nThat's not necessary right now " + myPet.name + " is perfectly healthy!");
                                 break;
                             }
-                        case 4:
 
-                            myPet.BoredomDecrease();
-                            Console.WriteLine("\nAw! " + myPet.name + " loves playing with you!");
-                            break;
-
-                        //TODO Add more cases for the other ways to interact with our pet
+                        /*End Game!
+                        -Chose to leave this as 10 so it is harder for the user to quit program by accident! */
 
                         case 10:
 
                             Console.WriteLine("\nThank you for playing.");
                             break;
+
+                        //Default
 
                         default:
 
@@ -99,12 +256,75 @@ namespace WeekProj2
                 }
 
 
-                //Put method calls here so the pet can have values change automatically.
 
+                //Pet Takes care of itself if: ...
+
+                while (myPet.tired >= 91)
+                {
+                    myPet.TiredDecrease();
+                    Console.WriteLine("\nLooks like poor " + myPet.name + " has fallen asleep on you! \nYou should remember to let your pet rest once in a while!");
+                    break;
+                }
+
+                while (myPet.potty >= 91)
+                {
+                    myPet.PottyDecrease();
+                    Console.WriteLine("\nOh No! It looks like " + myPet.name + " had an accident! \nMaybe you should have payed more attention to all of " + myPet.name + "'s needs?");
+                    break;
+                }
+
+                while (myPet.boredom >= 91)
+                {
+                    myPet.OverlyBored();
+                    Console.WriteLine("\nUh-oh! It looks like " + myPet.name + " has had fun tearing up the sofa! \nMaybe you should play with " + myPet.name + " more often?");
+                    break;
+                }
+
+
+
+                //Automatic Game over if: ...
+
+                while (myPet.health <= 9)
+                {
+                    Console.WriteLine("\nSorry, because you have failed to keep " + myPet.name + " healthy, the Animal Humane Society has decided you are not fit for a pet!  \nCharges will be filed!");
+                    selectedOption = myPet.end;
+                    break;
+                }
+
+                while (myPet.hunger >= 91)
+                {
+                    Console.WriteLine("\nPoor " + myPet.name + " is extreley malnourished! \nThe Animal Humane Society has decided you are not fit for a pet!  \nCharges will be filed!");
+                    selectedOption = myPet.end;
+                    break;
+                }
+
+                while (myPet.thirst >= 91)
+                {
+                    Console.WriteLine("\nPoor " + myPet.name + " is extreley dehydrated! \nThe Animal Humane Society has decided you are not fit for a pet!  \nCharges will be filed!");
+                    selectedOption = myPet.end;
+                    break;
+                }
+
+                while (myPet.restless >= 91)
+                {
+                    Console.WriteLine("\nOh NO! It appears you left the garage door open and " + myPet.name + " got out!  \nMaybe you should have walked " + myPet.name + " more often?");
+                    selectedOption = myPet.end;
+                    break;
+                }
+
+
+                
+
+
+                //Automatically changes values for pet.:
+
+                myPet.HealthDecrease();
                 myPet.HungerIncrease();
                 myPet.ThirstIncrease();
-                myPet.HealthDecrease();
                 myPet.BoredomIncrease();
+                myPet.RestlessIncrease();
+                myPet.TiredIncrease();
+                myPet.PottyIncrease();
 
 
             } while (selectedOption != 10);
--- WeekProj2/VirtualPet.cs
+++ WeekProj2/VirtualPet.cs
@@ -9,99 +9,236 @@ namespace WeekProj2
     class VirtualPet
     {
         public string name;
-        private int hunger;
-        private int thirst;
         public int health;
-        private int boredom;
+        public int hunger;
+        public int thirst;
+        public int boredom;
+        public int restless;
+        public int tired;
+        public int potty;
+        public int end;
 
         //Default Constructor
         public VirtualPet()
         {
             this.name = "Bob";
+            this.health = 100;
             this.hunger = 20;
             this.thirst = 20;
             this.boredom = 20;
-            this.health = 100;
-            //TODO account for more fields
+            this.restless = 20;
+            this.tired = tired = 20;
+            this.potty = 20;
+            this.end = 10;
         }
 
-        public VirtualPet(string name, int hunger, int thirst, int health, int sick, int boredom)
+        public VirtualPet(string name, int health, int hunger, int thirst, int boredom, int restless, int tired, int potty, int end)
         {
             this.name = name;
+            this.health = health;
             this.hunger = hunger;
             this.thirst = thirst;
-            this.health = health;
             this.boredom = boredom;
+            this.restless = restless;
+            this.tired = tired;
+            this.potty = potty;
+            this.end = end;
         }
 
-        //Increase the hunger
+        /*Health:
+         
+         Increase your pet's health through regular checkup's when health is below 50!
+         Vet visits cause a slight increase for all pet stats. 
+        -Taking care of your pet also slows health decrease!*/
+
+        public void HealthIncrease()
+        {
+            this.health = health + 55;
+            this.hunger = hunger - 5;
+            this.thirst = thirst - 5;
+            this.boredom = boredom - 4;
+            this.restless = restless - 4;
+            this.tired = tired - 4;
+            this.potty = potty - 4;
+        }
+
+        //Decrease the health:
+        public void HealthDecrease()
+        {
+            this.health = health - 4;
+        }
+
+
+        //Hunger:
+
+        //Increase the hunger:
         public void HungerIncrease()
         {
-            //Modify the values
             this.hunger = hunger + 3;
         }
 
-        //Decrease the hunger
-        public void HungerDecrease()
+        //Meal Time! -Decreases Hunger and Thirst by giving pet food + water, but increases tiredness:
+        public void Meal()
         {
-            //Modify the values
-            this.hunger = hunger - 5;
+            this.hunger = hunger - 6;
+            this.thirst = thirst - 4;
+            this.tired = tired + 2;
+            this.potty = potty + 3;
+            this.health = health + 2;
         }
 
-        //Increase the thirst
+
+        //Thirst:
+
+        //Increase the thirst:
         public void ThirstIncrease()
         {
-            //Modify the values
             this.thirst = thirst + 3;
         }
 
-        //Decrease the thirst
+        //Give your pet water to decrease thirst! -Also helps slow the increase of your pet's hunger!:
         public void ThirstDecrease()
         {
-            //Modify the values
-            this.thirst = thirst - 5;
+            this.thirst = thirst - 6;
+            this.hunger = hunger - 2;
+            this.potty = potty + 2;
+            this.health = health + 2;
         }
 
-        //Increase the health
-        public void HealthIncrease()
-        {
-            //Modify the values
-            this.health = health + 65;
-        }
 
-        //Decrease the health
-        public void HealthDecrease()
-        {
-            //Modify the values
-            this.health = health - 3;
-        }
+        //Boredom:
 
-        //Increase boredom
+        //Increase boredom:
         public void BoredomIncrease()
         {
-            //Modify the values
             this.boredom = boredom + 3;
         }
 
-        //Decrease boredom
+
+
+        //Play with your pet! Decreases Boredom and Restlessness, but increases thirst and tiredness.:
         public void BoredomDecrease()
         {
-            //Modify the values
-            this.boredom = boredom - 5;
+            this.boredom = boredom - 6;
+            this.restless = restless - 4;
+            this.thirst = thirst + 1;
+            this.tired = tired + 1;
+            this.health = health + 1;
+        }
+
+        public void OverlyBored()
+        {
+            this.boredom = boredom + 25;
+            this.restless = restless - 5;
+            this.thirst = thirst + 5;
+            this.tired = tired + 5;
+            this.health = health + 5;
         }
 
 
-       
+
+        //Restlessness:
+
+        //Increase restlessness of pet:
+
+        public void RestlessIncrease()
+        {
+            this.restless = restless + 3;
+        }
+
+        //Take your pet on a walk! Decreases restlessness and boredom of pet, but increases thirst and tiredness!:
+        public void RestlessDecrease()
+        {
+            this.restless = restless - 6;
+            this.boredom = boredom - 4;
+            this.thirst = thirst + 2;
+            this.tired = tired + 2;
+            this.health = health + 2;
+        }
+
+        //Take your pet on an extra long walk! Decreases restlessness and boredom of pet, but increases thirst and tiredness!:
+        public void OverlyRestless()
+        {
+            this.restless = restless + 25;
+            this.boredom = boredom - 5;
+            this.thirst = thirst + 5;
+            this.tired = tired + 5;
+            this.health = health + 5;
+        }
 
 
-        //Stats of virtual pet
+        //Tiredness:
+
+        //Increase tiredness of pet:
+        public void TiredIncrease()
+        {
+            this.tired = tired + 3;
+        }
+
+        //Pet takes quick nap (35 and above)! Decreases tiredness, restlessness and boredom of pet, but increases hunger and thirst!:
+
+        public void QuickNap()
+        {
+            this.tired = tired - 35;
+            this.restless = restless - 5;
+            this.boredom = boredom - 5;
+            this.hunger = hunger + 5;
+            this.thirst = thirst + 5;
+            this.health = health + 5;
+        }
+
+
+
+        //Let your pet sleep (50 and above) Decreases tiredness, restlessness and boredom of pet, but increases hunger and thirst!:
+        public void TiredDecrease()
+        {
+            this.tired = tired - 53;
+            this.restless = restless - 20;
+            this.boredom = boredom - 20;
+            this.hunger = hunger + 10;
+            this.thirst = thirst + 10;
+            this.health = health + 15;
+        }
+
+
+        //Bathroom Needs:
+
+        //Increase pets need to be let outside:
+
+        public void PottyIncrease()
+        {
+            this.potty = potty + 3;
+        }
+
+        //Let your pet outside (50 and above)! Decreases pet's bathroom needs!:
+
+        public void InAndOut()
+        {
+            this.potty = potty - 43;
+        }
+
+
+        public void PottyDecrease()
+        {
+            this.potty = potty - 55;
+            this.health = health + 1;
+        }
+
+
+
+        //Stats of pet:
+
         public void MyPetStatus()
         {
             Console.WriteLine(name);
             Console.WriteLine("Hunger: " + hunger);
             Console.WriteLine("Thirst: " + thirst);
-            Console.WriteLine("Health: " + health);
             Console.WriteLine("Boredom: " + boredom);
+            Console.WriteLine("Restlessness: " + restless);
+            Console.WriteLine("Tiredness: " + tired);
+            Console.WriteLine("Bathroom: " + potty);
+            Console.WriteLine("Health: " + health);
+
 
 
         }
